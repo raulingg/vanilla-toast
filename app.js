cfb279b05a58864156628812f7a0bfada45bbe6d
Raul Quispe (raulingg) <relaxedchild@gmail.com>
2020-08-15 18:34:19 -0500

    feat: Add a demo page

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-import toast from "./src/Toaster";
-import "./scss/main.scss";
+
+import toast from './src';
 
 const handleClick = (opts = {}) => toast.render(undefined, opts);
 
-document.addEventListener("click", (e) => {
-  if (e.target.dataset.position || e.target.dataset.type) {
+document.addEventListener('click', (e) => {
+    console.log(e.target.dataset)
+    if (e.target.dataset.position || e.target.dataset.type) {
+        console.log('entro')
     handleClick({ ...e.target.dataset });
   }
 });
